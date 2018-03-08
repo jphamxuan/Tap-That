2c4bcdc9d6563d1f83ed82ddced82be807c97659
jphamxuan <34908420+jphamxuan@users.noreply.github.com>
2018-03-07 21:22:17 -0800

    test

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,8 +3,15 @@
 
 
   // Side Nav Bar - Initialize collapse button
-  $(".button-collapse").sideNav();
+$(".button-collapse").sideNav();
 
-$$(document).ready(function() {
+$(document).ready(function() {
   $('.parallax').parallax();
 });
+
+$("#download-button").on("click", function(){
+  console.log("yay")
+  $(".input-field").empty();
+  $("#download-button").remove();
+  }
+);
