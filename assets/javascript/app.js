9785391c9657857583816e78ccb217d2a1fff2d5
jphamxuan <34908420+jphamxuan@users.noreply.github.com>
2018-03-14 17:01:59 -0700

    add petfinder and pawprint icon. still a but in more information filling div when next is clicked

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,112 +1,17 @@
-<<<<<<< HEAD
-var pfApiKey= "9da4a66757d1c617a44a579e1b05eabd" ;
+$(document).ready(function () {
 
-var cat
+    // Global Varriables //
+    var pfApiKey = "9da4a66757d1c617a44a579e1b05eabd";
+    var searchAnimal
+    var isAnimalselected = false
+    var isZipselected = false
 
-// the next line and function set up the button in our html to be clickable and reactive 
-document.addEventListener('DOMContentLoaded', bindButtons);
-function bindButtons(){
-    document.getElementById('submitZip').addEventListener('click', function(event){
-        event.preventDefault();
-        var zip = document.getElementById('zip').value; // this line gets the zip code from the form entry
-        var url = 'http://api.petfinder.com/pet.find';
-        
-        $.ajax({
-            url: url,
-            jsonp: "callback",
-            dataType: "jsonp",
-            data: {
-                key: pfApiKey,
-                animal: 'cat',
-                'location': zip,
-                output: 'basic',
-                format: 'json',
-                count: '5'
-            },
-            success: function( response ) {
-                console.log(response); // debugging
-                for (var i = 0; i < response.petfinder.pets.pet.length; i++) {
-                    cat= response.petfinder.pets.pet[i]
-                    var catName= response.petfinder.pets.pet[i].name.$t
-                    var catPhoto= cat.media.photos.photo[2].$t
-                    var catSex= cat.sex.$t
-                    var catDescription = cat.description.$t
-                    var catMix = cat.mix.$t
-                    var catAdd1 = cat.contact.address1.$t
-                    var catAdd2 = cat.contact.address2.$t
-                    var catEmail = cat.contact.email.$t
-                    var catPhone = cat.contact.phone.$t
-                    var catState = cat.contact.state.$t
-                    var catZip = cat.contact.zip.$t
-                    var catCity = cat.contact.city.$t
-                    var catUpdate = cat.lastUpdate.$t
-                    var catId = cat.id.$t
-                    var catSize = cat.size.$t
-                    // var catBreed= cat.breeds.breed[1].$t
-                    console.log(response.petfinder.pets.pet[i].name.$t)
-                    for (var j = 0; j < cat.breeds.breed.length; j++) {
-                        var catBreed= cat.breeds.breed[j].$t
-                    }
-                    $("#results").append("<div class='petDiv' id='"+ catName +"'><h1>" + catName +"</h1><img src='"+ catPhoto +"'><p>"+ catSex +", "+catBreed+", Is Cat a Mix? "+ catMix +"</p><p>"+catDescription+"</p></div>")
-                 }
-  // Initialize collapse button
-  $(".button-collapse").sideNav();
-  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
-  //$('.collapsible').collapsible();
-        
-=======
-// Side Nav Bar - Initialize collapse button
-$(".button-collapse").sideNav();
-
-$(document).ready(function() {
+    // Side Nav Bar - Initialize collapse button
+    $(".button-collapse").sideNav();
     $('.parallax').parallax();
     $('select').material_select();
-});
-
-// index java
-
-$("#download-button").on("click", function(event) {
-
-    event.preventDefault();
-    search()
-
-});
-
-function search() {
-    var searchTerm = $("#puppers").val();
-
-    var queryURL = "https://api.petfinder.com/pet.getRandom?format=json&key=9da4a66757d1c617a44a579e1b05eabd&arg1=foo";
-
-
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response) {
-        console.log(response)
-    }).catch(function(err) {
-        console.log(err)
-
-    })
-    $("#pet-choose").empty();
-    $("#button-row").empty();
-    var petPicture = $("<div class='col s6' id='pet-pic'></div>")
-    var petInfo = $("<div class='col s6' id='pet-info'></div>")
-    $("#pet-choose").append(petPicture, petInfo)
-    $("#button-row").append("<a href = '#'id = 'info-button'class = 'btn-large waves-effect waves-light orange'>More Information</a>")
-    $("#button-row").append("<a href = '#'id = 'next-button'class = 'btn-large waves-effect waves-light orange'>Next Pet</a>")
-}
-$(document).on('click', '#info-button', function() {
-    $("#pet-info").empty();
-
-})
-$(document).on('click', '#next-button', function() {
-    $("#pet-info").empty();
-
-})
-
-
-$(document).ready(function() {
+    $('.slideshow-container').hide();
+    // // Contact Page //
     // Initialize Firebase
     var config = {
         apiKey: "AIzaSyBgk9gIgJdoUNUBNVtMmjcTpyjrVaXcR8A",
@@ -122,7 +27,7 @@ $(document).ready(function() {
     // Side Nav Bar - Initialize collapse button
     $(".button-collapse").sideNav();
     var database = firebase.database();
-    $(document).on('click', '#send-button', function() {
+    $(document).on('click', '#send-button', function () {
         userId++
         database.ref('contact/' + userId).set({
             FirstName: $("#first_name").val().trim(),
@@ -135,5 +40,128 @@ $(document).ready(function() {
         $("#email").val('');
         $("#textarea1").val('');
     });
+//     // Functions when you click search //
+    $("#searchBTN").on("click", function (event) {
+        event.preventDefault();
+        searchAnimal = $("#petDropdown option:selected").attr('id');
+        // var selected = $("#petDropdown")
+        // var selectedChoice = selected.options[selected.selectedIndex].value;
+        if ($("#petDropdown option:selected").attr('id') == 'dog' || 'cat'){
+            isAnimalselected=true
+        }
+        zip = $("#zipcodes").val();
+        if (zip.length == 5){
+            isZipselected=true
+        }
+        if (isAnimalselected==true && isZipselected== true) {
+             search()
+        }   
+    });
+
+    document.addEventListener('DOMContentLoaded', search);
+    var slideIndex = 1;
+    // Search function that runs the Petfinder API and Ajax // 
+    function search() {
+        event.preventDefault();
+        $("#pet-choose").empty();
+        $("#button-row").empty();
+        $('#pet-choose').append("<div class='slideshow-container'><div id='slideshowResults'></div><a id='prev' class='prev' data-value='-1' >&#10094;</a><a class='next' data-value='1' id='next'>&#10095;</a></div>")
+        document.getElementById("next").addEventListener("click", plusSlides, false);
+        document.getElementById("prev").addEventListener("click", minusSlides, false);
+        var url = 'http://api.petfinder.com/pet.find';
+        $.ajax({
+            url: url,
+            jsonp: "callback",
+            dataType: "jsonp",
+            data: {
+                key: pfApiKey,
+                animal: searchAnimal,
+                'location': '92617',
+                output: 'basic',
+                format: 'json',
+                count: '20'
+            },
+            success: function (response) {
+                console.log(response);
+                for (var i = 0; i < response.petfinder.pets.pet.length; i++) {
+                    rescue = response.petfinder.pets.pet[i]
+                    var name = response.petfinder.pets.pet[i].name.$t
+                    var petPicture = rescue.media.photos.photo[2].$t
+                    var sex = rescue.sex.$t
+                    var description = rescue.description.$t
+                    var mix = rescue.mix.$t
+                    var address1 = rescue.contact.address1.$t
+                    var address2 = rescue.contact.address2.$t
+                    var email = rescue.contact.email.$t
+                    var phone = rescue.contact.phone.$t
+                    var state = rescue.contact.state.$t
+                    var zip = rescue.contact.zip.$t
+                    var city = rescue.contact.city.$t
+                    var lastUpdate = rescue.lastUpdate.$t
+                    var rescueId = rescue.id.$t
+                    var size = rescue.size.$t
+                    var age = rescue.age.$t
+                    // var rescueBreed= rescue.breeds.breed[1].$t
+                    if (rescue.breeds.breed.length > 1) {
+                        for (var j = 0; j < rescue.breeds.breed.length; j++) {
+                            var rescueBreed = rescue.breeds.breed[j].$t
+                        }
+                    }
+                    $('#slideshowResults').append("<div class='petDiv fade' id='" + name + "'><img src='" + petPicture + "' style='width:500px; height:500px;'><div class='text'><h1>" + name + "</h1><p>Sex: " + sex + "</p><p>Age: " + age + "</p><br><button id='moreBTN' value='"+ description +"' nameValue='"+ name +"' class='"+ name +" btn-large waves-effect waves-light orange'>More Information</button><div id=moreDiv>"+description+"<div></div><br></div>");
+                    $('#moreDiv').hide()
+                    $('.slideshow-container').show()
+                }
+                showSlides(slideIndex);
+                $("#moreBTN").on("click", function (event) {
+                    event.preventDefault();
+                    $("#moreBTN").hide();
+                    $('#moreDiv').show()
+                });
+            }
+        });
+    }
+    // Next control
+    function plusSlides() {
+        console.log('hello from plusSlides')
+        var n = $(this).data('value')
+        console.log('this is n within plusSlide', n)
+        console.log('this is slideIndex+=n ', slideIndex += n)
+        showSlides(slideIndex += n);
+    }
+    // previous control
+    function minusSlides() {
+        console.log('hello from minusSlides')
+        var n = -1
+        console.log('this is n within plusSlide', n)
+        console.log('this is slideIndex+=n ', slideIndex += n)
+        showSlides(slideIndex += n);
+    }
+    function currentSlide(n) {
+        console.log('within currentSlide ')
+        showSlides(slideIndex = n);
+    }
+    function showSlides(n) {
+        var i;
+        var slides = document.getElementsByClassName("petDiv");
+        console.log('hello from show slides');
+        console.log('slides.legnth is = ', slides.length);
+        console.log('this is n before if statements ', n);
+        if (n > slides.length) { slideIndex = 1 }
+        if (n < 1) { slideIndex = slides.length }
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        console.log('slides in showSlides', slides);
+        console.log('slideIndex ', slideIndex)
+        slides[slideIndex - 1].style.display = "block";
+    }
+
+    $(document).on('click', '#info-button', function () {
+        $("#pet-info").empty();
+        $("#moreDiv").hide();
+    })
+    $(document).on('click', '#next-button', function () {
+        $("#pet-info").empty();
+        $("#moreDiv").hide();
+    })
 });
->>>>>>> upstream/master
